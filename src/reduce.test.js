e37585c13d5230f24a140ca8967c13711fb22d14
Yaroslav <yaroslavhohulia@gmail.com>
2026-07-10 13:32:00 +0300

    added tests

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -11,7 +11,61 @@ describe('reduce', () => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {});
+  it('should be attached to Array prototype as a function', () => {
+    expect([].reduce2).toBeInstanceOf(Function);
+  });
 
-  // Add tests here
+  it('should correctly reduce an array when initial value is provided', () => {
+    const numbers = [1, 2, 3, 4];
+    const result = numbers.reduce2((acc, curr) => acc + curr, 10);
+
+    expect(result).toBe(20);
+  });
+
+  it(
+    'should use the first element as the initial value' +
+      'and start from index 1 if startValue is not provided',
+    () => {
+      const numbers = [1, 2, 3, 4];
+      const result = numbers.reduce2((acc, curr) => acc + curr);
+
+      expect(result).toBe(10);
+    },
+  );
+
+  it(
+    'should pass all 4 correct arguments' + 'to the callback on each iteration',
+    () => {
+      const arr = ['a'];
+      const callbackMock = jest.fn((acc, curr) => acc + curr);
+
+      arr.reduce2(callbackMock, 'start');
+
+      expect(callbackMock).toHaveBeenCalledWith('start', 'a', 0, arr);
+    },
+  );
+
+  it(
+    'should return the initial value immediately without calling the callback' +
+      'if the array is empty',
+    () => {
+      const emptyArray = [];
+      const callbackMock = jest.fn();
+      const result = emptyArray.reduce2(callbackMock, 100);
+
+      expect(result).toBe(100);
+      expect(callbackMock).not.toHaveBeenCalled();
+    },
+  );
+
+  it('should successfully handle complex object transformations', () => {
+    const fruits = ['apple', 'banana', 'apple'];
+    const result = fruits.reduce2((acc, fruit) => {
+      acc[fruit] = (acc[fruit] || 0) + 1;
+
+      return acc;
+    }, {});
+
+    expect(result).toEqual({ apple: 2, banana: 1 });
+  });
 });
